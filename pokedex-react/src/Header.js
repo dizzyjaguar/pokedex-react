56ed88f92cb6f219095729da8759e5b1ad3d7996
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-12 16:26:52 -0800

    create and style header

---
+++ pokedex-react/src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+      return (
+        <div id='header'>
+            <h1 id='logo-header'>Poke'dex</h1>
+        </div>
+      );
+    }
+    }
