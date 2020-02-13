d61d20537ac77c4c4d50c847b8befb69729695ea
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-12 17:20:52 -0800

    little more css

--- pokedex-react/src/PokeItem.js
+++ pokedex-react/src/PokeItem.js
@@ -4,8 +4,10 @@ export default class PokeItem extends Component {
     render() {
       return (
         <li className='poke-item'>
-            <h2>{this.props.pokeItem.pokemon}</h2>
-            <img alt='' src={this.props.pokeItem.url_image}></img>     
+            <h2 className='poke-name' >{this.props.pokeItem.pokemon}</h2>
+            <img alt='' src={this.props.pokeItem.url_image}></img>
+            <p> HP: {this.props.pokeItem.hp} </p>
+            <p> Element: {this.props.pokeItem.type_1} </p>     
         </li>
       );
     }
