b48f3d8bc775ad55e5133c94eef849f3738e416b
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-13 20:54:04 -0800

    add some css

--- pokedex-react/src/pokemon-api.js
+++ pokedex-react/src/pokemon-api.js
@@ -1,12 +1,12 @@
 
-export async function getPokemon() {
-    const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
-    let queryString = window.location.hash.slice(1);
+// export async function getPokemon() {
+//     const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+//     let queryString = window.location.hash.slice(1);
 
-    const url = `${URL}${queryString}`;
+//     const url = `${URL}${queryString}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-    return data;
-}
+//     return data;
+// }
