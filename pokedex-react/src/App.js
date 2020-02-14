d635c2ba775edb8bd898c1d2ab5b11b5a9d68777
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-13 20:39:54 -0800

    pokemon search is rendering, v1

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -2,25 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header.js'
 import PokeList from './PokeList.js'
+import SearchOptions from './SearchOptions.js'
+// import { getPokemon } from './pokemon-api';
 import request from 'superagent';
 
 export default class App extends Component {
   state = { pokedex: [] }
 
+  async loadPokemon() {
+    
+    const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+    let queryString = window.location.hash.slice(1);
+    const url = `${URL}?${queryString}`;
+
+    const searchedPokemon = await request.get(url);
+    this.setState({ pokedex: searchedPokemon.body.results }) 
+  }
+
   async componentDidMount() {
     const data = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex/')
     this.setState({ pokedex: data.body.results })
-
-    console.log(data.body)
+    
+    window.addEventListener('hashchange', () => {
+      this.loadPokemon()
+    })
   }
 
 
 
   render() {
+    
     return (
       <div>
 
       <Header></Header>
+      <SearchOptions></SearchOptions>
+      
       <PokeList pokedex={this.state.pokedex}></PokeList>
       </div>
     );
---
+++ pokedex-react/src/pokemon-api.js
@@ -0,0 +1,12 @@
+
+export async function getPokemon() {
+    const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+    let queryString = window.location.hash.slice(1);
+
+    const url = `${URL}${queryString}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
